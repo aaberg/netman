2e54bab85272002a2a329e8d69df2d059774d4ef
Lars Aaberg <lars@aaberg.cc>
2025-12-07 21:32:28 +0100

    Run prettier

--- netman-web/src/routes/app/[tenant]/contacts/new/+page.server.ts
+++ netman-web/src/routes/app/[tenant]/contacts/new/+page.server.ts
@@ -1,17 +1,17 @@
-import type {Actions} from "./$types";
-import {redirect} from "@sveltejs/kit";
-import type {ContactWithDetails} from "$lib/contactModel";
-import {saveContact} from "$lib/server/contact";
-import {accessToken} from "$lib/server/common";
+import type { Actions } from "./$types"
+import { redirect } from "@sveltejs/kit"
+import type { ContactWithDetails } from "$lib/contactModel"
+import { saveContact } from "$lib/server/contact"
+import { accessToken } from "$lib/server/common"
 
 export const actions = {
-    default: async  ({request, cookies, params} ) => {
-        const { tenant } = params
-        const form = await request.formData()
-        const contactData = form.get('contact') as string
-        const contact = JSON.parse(contactData) as ContactWithDetails
+  default: async ({ request, cookies, params }) => {
+    const { tenant } = params
+    const form = await request.formData()
+    const contactData = form.get("contact") as string
+    const contact = JSON.parse(contactData) as ContactWithDetails
 
-        await saveContact(accessToken(cookies), tenant, contact)
-        return redirect(303, `/app/${tenant}/contacts`)
-    }
+    await saveContact(accessToken(cookies), tenant, contact)
+    return redirect(303, `/app/${tenant}/contacts`)
+  }
 } satisfies Actions
--- netman-web/vite.config.ts
+++ netman-web/vite.config.ts
@@ -1,12 +1,12 @@
-import tailwindcss from '@tailwindcss/vite';
-import { svelteTesting } from '@testing-library/svelte/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite"
+import { svelteTesting } from "@testing-library/svelte/vite"
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-    server: {
-        host: '127.0.0.1',
-        port: 5173,
-    }
-});
+  plugins: [tailwindcss(), sveltekit()],
+  server: {
+    host: "127.0.0.1",
+    port: 5173
+  }
+})

55a4c7f5a582fb5fa7004cc328914db60f98a380
Lars Aaberg <lars@aaberg.cc>
2025-12-09 22:19:10 +0100

    Added some configuration, so that vitest won't try to run playwright tests

--- netman-web/vite.config.ts
+++ netman-web/vite.config.ts
@@ -8,5 +8,10 @@ export default defineConfig({
   server: {
     host: "127.0.0.1",
     port: 5173
+  },
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup-client.ts"]
   }
 })

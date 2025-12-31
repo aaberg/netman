461c771025179d8aa1cac855952bcd103b52da71
Lars Aaberg <lars@aaberg.cc>
2025-12-31 11:00:31 +0100

    Added otel to api

---
+++ netman-api/src/main/kotlin/netman/configuration/OpenTelemetryConfig.kt
@@ -0,0 +1,26 @@
+package netman.configuration
+
+import com.azure.monitor.opentelemetry.autoconfigure.AzureMonitorAutoConfigure
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Primary
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.annotation.Value
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk
+import jakarta.inject.Singleton
+
+@Factory
+class OpenTelemetryConfig {
+
+    @Singleton
+    @Primary
+    @Requires(property = "otel.exporter.azuremonitor.enabled", value = "true")
+    fun azureMonitorExporter(@Value("\${otel.exporter.azuremonitor.connection-string}") connectionString: String)
+    : OpenTelemetry {
+        val otelBuilder = AutoConfiguredOpenTelemetrySdk.builder()
+
+        AzureMonitorAutoConfigure.customize(otelBuilder, connectionString)
+
+        return otelBuilder.build().openTelemetrySdk
+    }
+}

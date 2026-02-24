a7aa12287966a2c5003e0c38893731160ca53633
Lars Aaberg <lars@aaberg.cc>
2026-02-24 17:20:56 +0100

    changed strategy to using a dockerfile using prebuild jar

---
+++ netman-api/prebuild.Dockerfile
@@ -0,0 +1,11 @@
+FROM eclipse-temurin:25-jre-alpine
+
+# Set working directory
+WORKDIR /app
+
+COPY ./build/libs/netman-api-0.1-SNAPSHOT-all.jar app.jar
+
+EXPOSE 8080
+
+# Set the entrypoint to run the application
+ENTRYPOINT ["java", "-jar", "./app.jar"]
--- netman-web/playwright.config.ts
+++ netman-web/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@playwright/test"
 
 export default defineConfig({
   webServer: {
-    command: "npm run build && dotenv -e .env npm run preview ",
+    command: "npm run build && dotenv -e .env.backend npm run preview ",
     port: 4173
   },
   testDir: "e2e",

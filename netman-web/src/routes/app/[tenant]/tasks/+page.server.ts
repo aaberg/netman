61eb5449a1f1448dfb836563e7d3d557db4bde74
Lars Aaberg <lars@aaberg.cc>
2026-03-04 16:13:00 +0100

    Removed unused import

--- netman-web/src/routes/app/[tenant]/tasks/+page.server.ts
+++ netman-web/src/routes/app/[tenant]/tasks/+page.server.ts
@@ -1,7 +1,6 @@
 import type { PageServerLoad } from "./$types"
 import { accessToken } from "$lib/server/common"
 import { getFollowUps, getActions } from "$lib/server/followUp"
-import { getTenantSummary } from "$lib/server/tenantSummary"
 
 export const load: PageServerLoad = async ({ cookies, params }) => {
   const { tenant } = params

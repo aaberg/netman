3fc5c5da7e5dcec271a61c6b161f900e8a7469f7
Lars Aaberg <lars@aaberg.cc>
2026-03-20 07:53:33 +0100

    renamed the last communication references to interaction

--- netman-api/src/main/kotlin/netman/access/repository/InteractionRepository.kt
+++ netman-api/src/main/kotlin/netman/access/repository/InteractionRepository.kt
@@ -1,18 +1,16 @@
 package netman.access.repository
 
 import io.micronaut.data.jdbc.annotation.JdbcRepository
-import io.micronaut.data.model.Page
-import io.micronaut.data.model.Pageable
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.GenericRepository
 import java.util.*
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
 interface InteractionRepository : GenericRepository<InteractionDTO, UUID> {
-    fun save(communication: InteractionDTO): InteractionDTO
+    fun save(interaction: InteractionDTO): InteractionDTO
     fun getById(id: UUID): InteractionDTO?
     fun findByContactIdOrderByTimestampDesc(contactId: UUID): List<InteractionDTO>
     fun deleteById(contactId: UUID)
-    fun update(communication: InteractionDTO): InteractionDTO
+    fun update(interaction: InteractionDTO): InteractionDTO
     fun existsById(id: UUID): Boolean
 }

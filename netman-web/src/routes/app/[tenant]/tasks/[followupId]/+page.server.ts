629135d7fb1c310fb284be269c1f3b1a73382692
Lars Aaberg <lars@aaberg.cc>
2026-04-12 16:32:26 +0200

    Removed the old part of the application that I'm rewriting

--- netman-web/src/routes/app/[tenant]/tasks/[followupId]/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import type { PageServerLoad } from "./$types"
-
-export const load: PageServerLoad = async ({ params }) => {
-  const { tenant, followupId } = params
-  
-  return { tenant, followupId }
-}
